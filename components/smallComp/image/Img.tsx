4ae0528ef6b70793583ad24a88a6180901558096
asar109 <themernist@gmail.com>
2024-02-29 15:44:54 +0500

    delete unnecessary stuff

--- components/smallComp/image/Img.tsx
+++ components/smallComp/image/Img.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Img(props) {
-  return (
-
-        // eslint-disable-next-line @next/next/no-img-element
-        <img src={props.src} alt={props.alt} className={props.className} />
-  )
+  return <img src={props.src} alt={props.alt} className={props.className} />;
 }

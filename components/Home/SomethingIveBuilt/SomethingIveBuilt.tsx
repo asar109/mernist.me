e29246110047c6639c19d8751b6f3350d17c0b19
asar109 <themernist@gmail.com>
2024-03-07 00:14:12 +0500

    add discord

--- components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
+++ components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
@@ -222,12 +222,118 @@ export default function SomethingIveBuilt() {
                 className="flex flex-wrap w-full text-gray-300 md:text-gray-400
                text-sm font-Text2 md:justify-start"
               >
+                <span className="pr-4 z-10">HTML</span>
+                <span className="pr-4 z-10">CSS</span>
+                <span className="pr-4 z-10">Javascript</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 ">
+                <GithubIcon link="https://github.com/asar109/app_landing_page" />
+                <a
+                  href="https://food-web-mernsit.netlify.app/"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
+                  <ExternalLink url={""} router={router} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
 
-                <span className="pr-4 z-10">Next.js</span>
-                <span className="pr-4 z-10">Typescript</span>
+        {/* // ?  Project  3*/}
+        <div
+          data-aos="fade-up"
+          className="relative md:grid md:grid-cols-12 w-full md:h-96 "
+        >
+          {/* Left image */}
+          <div
+            className="hidden bg-primary z-10  py-4 
+          absolute md:grid grid-cols-12 w-full h-full  content-center"
+          >
+            <div className="relative rounded-md w-full h-full col-span-7 ">
+              <a
+                href="https://mernist.live/"
+                target={"_blank"}
+                rel="noreferrer"
+              >
+                <div
+                  className="absolute w-full h-full rounded bg-primary 
+           transition-opacity opacity-30 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </a>
+
+              <Img
+                src={"/projects/discord.PNG"}
+                alt={"Project Screen shot"}
+                className={`w-full rounded h-full `}
+              />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-secondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-primary opacity-80 z-10"></div>
+                <Img
+                  src={"/projects/discord.PNG"}
+                  alt={"Project Screen shot"}
+                  className={`w-full h-full `}
+                />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5 
+            col-span-8 flex flex-col items-start md:items-end space-y-3"
+            >
+              <div className="flex flex-col space-y-1 md:items-end z-10">
+                <span className="text-secondary text-base">
+                  Mernist Discord
+                </span>
+                <a
+                  href="http://mernist.live/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className=" md:text-gray-200 text-secondary font-bold text-xl hover:cursor-pointer">
+                    Real time chat application
+                  </span>
+                </a>
+              </div>
+              <div className="w-full md:bg-tertiary rounded-md py-6 md:p-6  z-10">
+                {" "}
+                <p className="text-gray-300 md:text-gray-400 text-left ">
+                  I developed a dynamic chat application similar to{" "}
+                  <span className="text-secondary">Discord.</span> Users can
+                  effortlessly create{" "}
+                  <span className="text-secondary">servers </span>
+                  and manage member access. Each server supports{" "}
+                  <span className="text-secondary">text,</span>{" "}
+                  <span className="text-secondary">voice,</span>
+                  and <span className="text-secondary">video </span>
+                  channels for seamless communication. Leveraging{" "}
+                  <span className="text-secondary">Socket.IO </span>
+                  for real-time messaging ensures instant conversations, with a{" "}
+                  <span className="text-secondary">fallback mechanism </span>
+                  for continuous functionality. This project highlights my
+                  expertise in building interactive web applications focused on
+                  user experience and real-time communication.{" "}
+                </p>
+                ;
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-end"
+              >
+                <span className="pr-4 z-10">Next.Js</span>
+                <span className="pr-4 z-10">Socket.io</span>
                 <span className="pr-4 z-10">Prisma</span>
                 <span className="pr-4 z-10">MySQL</span>
-                <span className="pr-4 z-10">Clerk Auth</span>
+                <span className="pr-4 z-10">LiveKit</span>
+                <span className="pr-4 z-10">Clerk</span>
               </ul>
               <div className="z-10 flex fle-row space-x-5 ">
                 <GithubIcon link="https://github.com/asar109/discord-mernist" />
